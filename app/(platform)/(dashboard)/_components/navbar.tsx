dbb0e7b2f011b7fb87beb9b371f2fe46668fb848
hai.nguyen <nguyenthanhhai200092@gmail.com>
2023-12-10 00:37:08 +0700

    Add side bar, responsive and skeleton effect

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -2,14 +2,15 @@ import { Plus } from "lucide-react";
 import Logo from "@/components/logo";
 import { Button } from "@/components/ui/button";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { MobileSidebar } from "./mobile-sidebar";
 
 export const Navbar = () => {
   return (
     <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
-      {/* TODO: Mobile Sidebar */}
+      <MobileSidebar />
       <div className="flex items-center gap-x-4">
         <div className="hidden md:flex">
-          <Logo></Logo>
+          <Logo />
         </div>
         <Button
           variant="primary"
---
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,12 @@
+import { OrgControl } from "./_components/org-control";
+
+const OrganizationLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <OrgControl />
+      {children}
+    </>
+  );
+};
+
+export default OrganizationLayout;
