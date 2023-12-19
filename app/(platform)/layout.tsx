edd8e05d676f334f101c94f6b2ad160a1402ba00
hai.nguyen <nguyenthanhhai200092@gmail.com>
2023-12-19 22:46:52 +0700

    Create Board Popover Form

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,13 +1,13 @@
-import { ClerkProvider } from "@clerk/nextjs"
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
-const PlatformLayout = ({
-  children
-}: { children: React.ReactNode }) => {
-    return (
-      <ClerkProvider>
-        {children}
-      </ClerkProvider>
-    )
-}
+const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ClerkProvider>
+      <Toaster />
+      {children}
+    </ClerkProvider>
+  );
+};
 
 export default PlatformLayout;
