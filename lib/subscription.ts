1668aea507ad28b3e50e59c98b42ccf2becd5897
hai.nguyen <nguyenthanhhai200092@gmail.com>
2023-12-24 02:52:02 +0700

    Update some variables

--- lib/subscription.ts
+++ lib/subscription.ts
@@ -3,7 +3,7 @@ import { auth } from "@clerk/nextjs";
 
 import { db } from "@/lib/db";
 
-const DAY_IN_MS = 84_400_000;
+const DAY_IN_MS = 86_400_000;
 
 export const checkSubscription = async () => {
   const { orgId } = auth();
